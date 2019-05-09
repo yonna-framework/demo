8e144438ca204d2a7a353be13ad4b05ad89c266d
马志洋 <mazhiyang@shein.com>
2019-05-09 13:09:10 +0800

    up

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$core = new HunzsigServer/PhpureCore();
-var_dump($core);
-
-$core = new Vlucas/Phpdotenv();
+$core = new Dotenv();
 var_dump($core);
 var_dump(getenv('APP_NAME'));
