970305389521502ed197512e6cd32c7271a35b65
hunzsig <mzyhaohaoren@qq.com>
2019-05-14 22:46:37 +0800

    up

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-// 加载composer模块
+/**
+ * 加载 composer 模块
+ * 如果报错请安装 composer 并执行 composer install 安装所需要的包库
+ *
+ * Load composer modules
+ * Install composer And exec composer install in your shell when throw error.
+ */
 require __DIR__ . '/vendor/autoload.php';
 
 use PhpureCore\Bootstrap;
