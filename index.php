99e3611176e3bcce311adae6e070024772760d6f
马志洋 <mazhiyang@shein.com>
2019-05-14 17:41:20 +0800

    up

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
+// 加载composer模块
 require __DIR__ . '/vendor/autoload.php';
+
+use PhpureCore\Bootstrap;
 
 $Dotenv = Dotenv\Dotenv::create(__DIR__);
 $Dotenv->load();
+
+dump($_SERVER,$_SERVER);
 
 var_dump(getenv('APP_NAME'));
