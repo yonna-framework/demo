9af14d1fc754713d2b8f3e75cefc887106aeaba5
马志洋 <mazhiyang@shein.com>
2019-05-21 16:26:48 +0800

    up

--- index.php
+++ index.php
@@ -4,14 +4,16 @@
  * 如果报错请安装 composer 并执行 composer install 安装所需要的包库
  *
  * Load composer modules
- * Install composer And exec composer install in your shell when throw error.
+ * Install composer and exec composer install in your shell when error throw.
  */
 require __DIR__ . '/vendor/autoload.php';
 
-PhpureCore\Container::singleton(PhpureCore\Interfaces\Bootstrap::class,
+
+PhpureCore\Core::bootstarp(
     realpath(__DIR__),
     'example',
     PhpureCore\Mapping\BootType::AJAX_HTTP
 );
+
 
 var_dump('index.php: end');
