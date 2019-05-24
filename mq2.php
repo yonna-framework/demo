fb72e720df3f895a1f3477ac62ef186eeb6d884a
马志洋 <mazhiyang@shein.com>
2019-05-24 14:19:16 +0800

    mq

--- mq2.php
+++ mq2.php
@@ -3,19 +3,33 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
-$channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+class mq2
+{
 
-echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
-$callback = function ($msg) {
-    echo " [x] Received ", $msg->body, "\n";
-};
+    function __construct($utime = 1000)
+    {
+        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
 
-//在接收消息的时候调用$callback函数
-$channel->basic_consume('hello', '', false, true, false, false, $callback);
+        $channel->queue_declare('hello', false, false, false, false);
 
-while (count($channel->callbacks)) {
-    $channel->wait();
+        echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+        $callback = function ($msg) use ($utime) {
+            // usleep($utime);
+            echo " [x] Received ", $msg->body, "\n";
+        };
+
+        //在接收消息的时候调用$callback函数
+        $channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+        while (count($channel->callbacks)) {
+            $channel->wait();
+        }
+    }
+
 }
+
+$params = getopt('u:');
+$u = $params['u'] ?? 1000;
+(new mq2($u));
