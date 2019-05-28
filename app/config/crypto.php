2a4623216b00a8104d36d1c0f894068ba8987e8b
hunzsig <mzyhaohaoren@qq.com>
2019-05-28 21:58:44 +0800

    up

--- app/config/crypto.php
+++ app/config/crypto.php
@@ -2,17 +2,17 @@
 
 use PhpureCore\Config\Crypto;
 
-Crypto::set('io_token', getenv('CRYPTO_IO_TOKEN'));
-Crypto::set('io_token_secret', getenv('CRYPTO_IO_TOKEN_SECRET'));
-Crypto::set('io_request_protocol', getenv('CRYPTO_IO_REQUEST_PROTOCOL'));
-Crypto::set('io_request_type', getenv('CRYPTO_IO_REQUEST_TYPE'));
-Crypto::set('io_request_secret', getenv('CRYPTO_IO_REQUEST_SECRET'));
-Crypto::set('io_request_iv', getenv('CRYPTO_IO_REQUEST_IV'));
+Crypto::set('io_token', Crypto::env('CRYPTO_IO_TOKEN'));
+Crypto::set('io_token_secret', Crypto::env('CRYPTO_IO_TOKEN_SECRET'));
+Crypto::set('io_request_protocol', Crypto::env('CRYPTO_IO_REQUEST_PROTOCOL'));
+Crypto::set('io_request_type', Crypto::env('CRYPTO_IO_REQUEST_TYPE'));
+Crypto::set('io_request_secret', Crypto::env('CRYPTO_IO_REQUEST_SECRET'));
+Crypto::set('io_request_iv', Crypto::env('CRYPTO_IO_REQUEST_IV'));
 
-Crypto::set('package_type', getenv('CRYPTO_PACKAGE_TYPE'));
-Crypto::set('package_secret', getenv('CRYPTO_PACKAGE_SECRET'));
-Crypto::set('package_iv', getenv('CRYPTO_PACKAGE_IV'));
+Crypto::set('package_type', Crypto::env('CRYPTO_PACKAGE_TYPE'));
+Crypto::set('package_secret', Crypto::env('CRYPTO_PACKAGE_SECRET'));
+Crypto::set('package_iv', Crypto::env('CRYPTO_PACKAGE_IV'));
 
-Crypto::set('db_type', getenv('CRYPTO_DB_TYPE'));
-Crypto::set('db_secret', getenv('CRYPTO_DB_SECRET'));
-Crypto::set('db_iv', getenv('CRYPTO_DB_IV'));
+Crypto::set('db_type', Crypto::env('CRYPTO_DB_TYPE'));
+Crypto::set('db_secret', Crypto::env('CRYPTO_DB_SECRET'));
+Crypto::set('db_iv', Crypto::env('CRYPTO_DB_IV'));
--- app/config/database.php
+++ app/config/database.php
@@ -5,26 +5,28 @@ use PhpureCore\Config\Database;
 
 Database::mysql(
     'default', [
-        'host' => getenv('DB_MY_1_HOST'),
-        'port' => getenv('DB_MY_1_PORT'),
-        'account' => getenv('DB_MY_1_ACCOUNT'),
-        'password' => getenv('DB_MY_1_PASSWORD'),
-        'name' => getenv('DB_MY_1_NAME'),
-        'charset' => getenv('DB_MY_1_CHARSET'),
-        'auto_cache' => getenv('DB_MY_1_AUTO_CACHE')
+        'host' => Database::env('DB_MY_1_HOST'),
+        'port' => Database::env('DB_MY_1_PORT'),
+        'account' => Database::env('DB_MY_1_ACCOUNT'),
+        'password' => Database::env('DB_MY_1_PASSWORD'),
+        'name' => Database::env('DB_MY_1_NAME'),
+        'charset' => Database::env('DB_MY_1_CHARSET'),
+        'auto_cache' => Database::env('DB_MY_1_AUTO_CACHE')
     ]
 );
 
 Database::redis(
     'redis', [
-        'host' => getenv('DB_REDIS_1_HOST'),
-        'port' => getenv('DB_REDIS_1_PORT')
+        'host' => Database::env('DB_REDIS_1_HOST'),
+        'port' => Database::env('DB_REDIS_1_PORT'),
+        'password' => Database::env('DB_REDIS_1_PASSWORD')
     ]
 );
 
 Database::redis(
     'cache', [
-        'host' => getenv('DB_REDIS_1_HOST'),
-        'port' => getenv('DB_REDIS_1_PORT')
+        'host' => Database::env('DB_REDIS_1_HOST'),
+        'port' => Database::env('DB_REDIS_1_PORT'),
+        'password' => Database::env('DB_REDIS_1_PASSWORD')
     ]
 );
