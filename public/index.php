aa11aca72d487ec1f85437af55605cd94afdbf8e
hunzsig <mzyhaohaoren@qq.com>
2019-05-30 07:56:28 +0800

    index moveto public

--- public/index.php
+++ public/index.php
@@ -6,10 +6,12 @@
  * Load composer modules
  * Install composer and exec composer install in your shell when error throw.
  */
-require __DIR__ . '/vendor/autoload.php';
+
+
+require __DIR__ . '/../vendor/autoload.php';
 
 PhpureCore\Core::bootstrap(
-    realpath(__DIR__ ),
+    realpath(__DIR__ . '/../'),
     'example',
     PhpureCore\Mapping\BootType::AJAX_HTTP
 );
